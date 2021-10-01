12c8ee1948b5a0a4c260ff4f172263bc588cc3cd
JuanChaves91 <90256152+JuanChaves91@users.noreply.github.com>
2021-10-01 12:09:05 -0500

    login

--- Vita-Family-Gforce/vita-family/src/login/LoginPage.jsx
+++ Vita-Family-Gforce/vita-family/src/login/LoginPage.jsx
@@ -1,13 +1,33 @@
 import React from 'react';
-import logo from './logo.jpeg'
+import logo from './vitafamily-logo.png'
 
 
 function LoginPage(){
 
     return(
         <div> 
-            <img src={logo} className= "imagen"/>
-            <h1 className = "login-title" >Pagina de Log in</h1>
+            <div class="contenedor">
+                <img class="vflogo" src={logo} alt="VitaFamily">
+                <div class="google-btn">
+                    <div class="google-icon-wrapper">
+                        <img class="google-icon" src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"/>
+                    </div>
+                    <a href="#" class="btn-text"><b>Iniciar sesión con Google</b></a>
+                </div>
+                <form class="form" id="login-form">
+                    <div class="form-div">
+                        <label for="user">Correo</label>
+                        <input type="text" id="user" autocomplete="off">
+                    </div>
+                    <div class="form-div">
+                        <label for="password">Contraseña</label>
+                        <input type="password" id="password">
+                    </div>
+                    <div class="form-div" id="submit">
+                        <button type="submit" name="login" form="login-form">Iniciar sesión</button>
+                    </div>
+                </form>
+            </div>
         </div>
     )
 }
