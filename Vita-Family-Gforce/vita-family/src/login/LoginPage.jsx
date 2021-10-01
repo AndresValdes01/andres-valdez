ebc311acd38cce0f153cdb3d2addd53601977610
MiguelG08 <giraldomiguel19@gmail.com>
2021-10-01 12:57:18 -0500

    añadí el cierre de algunas etiquetas

--- Vita-Family-Gforce/vita-family/src/login/LoginPage.jsx
+++ Vita-Family-Gforce/vita-family/src/login/LoginPage.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import logo from './vitafamily-logo.png'
+import "./loginStyles.css"
 
 
 function LoginPage(){
@@ -7,7 +8,7 @@ function LoginPage(){
     return(
         <div> 
             <div class="contenedor">
-                <img class="vflogo" src={logo} alt="VitaFamily">
+                <img class="vflogo" src={logo} alt="VitaFamily"/>
                 <div class="google-btn">
                     <div class="google-icon-wrapper">
                         <img class="google-icon" src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"/>
@@ -17,11 +18,11 @@ function LoginPage(){
                 <form class="form" id="login-form">
                     <div class="form-div">
                         <label for="user">Correo</label>
-                        <input type="text" id="user" autocomplete="off">
+                        <input type="text" id="user" autocomplete="off"/>
                     </div>
                     <div class="form-div">
                         <label for="password">Contraseña</label>
-                        <input type="password" id="password">
+                        <input type="password" id="password"/>
                     </div>
                     <div class="form-div" id="submit">
                         <button type="submit" name="login" form="login-form">Iniciar sesión</button>
