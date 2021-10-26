ef34c7fc24de0f459aaf75bad74212d731ec45f1
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2021-10-26 11:26:57 -0500

    [+] Product Page: Redefine como Funcional Component

--- Vita-Family-Gforce/vita-family/src/products/ProductPage.jsx
+++ Vita-Family-Gforce/vita-family/src/products/ProductPage.jsx
@@ -1,108 +1,134 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from './logo.jpeg';
 import "./productsstyle.css";
 import axios from 'axios';
 
-class ProductPage extends React.Component{
-  state = {
-    productos : [],
-    productoSeleccionado : null
-  }
+// Defino Componente Funcional
+const ProductPage = () => {
 
+    // Defino State del Componente
+    const [ formData, setFormData ] = useState({
+        descripcion: null,
+        precio: 0,
+        cantidad: 0,
+        estado: null
+    });
+    const [ productos, setProductos ] = useState([]);
 
- 
-  componentDidMount(){
-    axios.get('http://localhost:3001/api/productos')
-    .then(res=>{
-      const productos = res.data.productos;
-      this.setState({productos});
-    })
-  }
-     registrarProducto(event){
-      event.preventDefault();
-  
-      //consumir post con axios
-      axios.post('http://localhost:3001/api/productos',{
-        id: event.target.id.value,  
-        Descripcion: event.target.Descripcion.value,          
-        preciounidad: event.target.preciounidad.value,
-        cantidad: event.target.cantidad.value,
-        estado: event.target.estado.value
+    const { description, precio, cantidad, estado } = formData;       //  Desestructura la data del State
 
-      }).then(res =>{ 
-         alert(res.data.message);
-        console.log(res.data);
-      })
-    
+    // Maneja el envio de la data del formulario
+    const handleSubmit = ( event ) => {
+        event.preventDefault();
+        console.log( formData );
     }
 
-    // Actualizar
-    handleActualizar = ( event, id ) => {
-      axios.update( `http://localhost:3001/api/productos/${ id }`, {
-        descripcion: event.target.Descripcion.value,
-        precioUnidad: event.target.preciounidad.value,
-        cantidad: event.target.cantidad.value,
-        estado: event.target.estado.value
-      } ).then(res =>{
-        alert(res.data.message);
-        console.log(res.data);
-     })
+    // Maneja los cambios en la data de los campos y establece el estado del componente
+    const handleChange = ( event ) => {
+
+        // Funcion del State con el que cambiamos el estado del componente
+        setFormData({
+            ...formData,
+            [ event.target.name ]: event.target.value
+        })
     }
-  
-    render(){
-  return(
-      <div>
-      <div>
-        <img src={logo} className= "logo.jpep" className="imagen"/>
-        <h1 className= "h1products">Gestión de productos</h1>
-      </div>
-      <div className ="cont">
-      <form onSubmit ={this.registrarProducto} className = "fproduct">
-            <input name ="id" className="registro" type="text" placeholder="Numero de producto"/>
-            <input name ="Descripcion" className="registro" type="text" placeholder="Nuevo producto"/>
-            <input name ="preciounidad" className="registro" type="number" placeholder="precio unidad"/><br/>
-            <input name="cantidad" className="registro" type="number" placeholder="cantidad"/>
-            <select name="estado" id="selectorRol">
-                <option >Disponible</option>
-                <option >No Disponible</option>
-            </select><br/>
-            <button className="bt-product" type="submit" >Registrar producto</button>
-            <button type="reset"> Cancelar</button>
-        </form>
-      </div>
-    <div>
-        <form>
-          <input type="search" id="Bucador" placeholder="Buscar"/>
-          <input
-          type="date"
-          className="form-control"
-          placeholder=" Fecha del registro" />
-        </form>
-    </div>
-      <table className= "tproducts">
-        <thead className ="theadproducts">
-          <tr className="trproducts">
-            <th className ="thproducts">Precio unitario</th>
-            <th className ="thproducts">Cantidad</th>
-            <th className ="thproducts">Descripción producto</th>
-            <th className ="thproducts">estado</th>
-            <th className ="thproducts">Acción</th>
-          </tr>
-        </thead>
-        {this.state.productos.map(
-          producto =>
-        <tr className="trproducts">
-          <td className ="tdproducts">{producto.preciounidad}</td>
-          <td className ="tdproducts">{producto.cantidad}</td>
-          <td className ="tdproducts">{producto.Descripcion}</td>
-          <td className ="tdproducts">{producto.estado}</td>
-          <td className ="tdproducts"><button>borrar</button><button onClick={ this.handleActualizar( producto._id ) }>Actualizar</button></td>
-          </tr>
-      )}
-           </table>
-</div>
-  );
-}
+
+    return (
+        <>
+            <div>
+                <img src={logo} alt="Logo" className="imagen" />
+                <h1 className= "h1products">Gestión de productos</h1>
+            </div>
+            <div className ="cont">
+                <form
+                    onSubmit={ handleSubmit }
+                    className="fproduct"
+                >
+                    <div className="form-fields">
+                        <div className="form-field">
+                            <label htmlFor="descripcion">Descripción</label>
+                            <input
+                                type="text"
+                                placeholder="Nuevo producto"
+                                name="descripcion"              //  String/Nombre del campo, con el mismo nombre de la variable que viene de la desestructuracion
+                                value={ description }           //  Variable viene de la desestructuracion
+                                onChange={ handleChange }       //  Closure/Funcion que actua ante los cambios
+                            />
+                        </div>
+                        <div className="form-field">
+                            <label htmlFor="precio">Precio unitario</label>
+                            <input
+                                type="number"
+                                placeholder="Ej: 400"
+                                name="precio"                   //  String/Nombre del campo, con el mismo nombre de la variable que viene de la desestructuracion
+                                value={ precio }                //  Variable viene de la desestructuracion
+                                onChange={ handleChange }       //  Closure/Funcion que actua ante los cambios
+                            />
+                        </div>
+                        <div className="form-field">
+                            <label htmlFor="cantidad">Cantidad</label>
+                            <input
+                                type="number"
+                                placeholder="cantidad"
+                                name="cantidad"                 //  String/Nombre del campo, con el mismo nombre de la variable que viene de la desestructuracion
+                                value={ cantidad }              //  Variable viene de la desestructuracion
+                                onChange={ handleChange }       //  Closure/Funcion que actua ante los cambios
+                            />
+                        </div>
+                        <div className="form-field">
+                            <label htmlFor="estado">Estado</label>
+                            <select
+                                name="estado"
+                                value={ estado }
+                                onChange={ handleChange }
+                            >
+                                <option value="">Seleccione...</option>
+                                <option value="disponible">Disponible</option>
+                                <option value="no disponible">No Disponible</option>
+                            </select><br/>
+                        </div>
+                    </div>
+                    <div className="form-buttons">
+                        <button
+                            className="btn btn-submit bt-product"
+                            type="submit"
+                        >Registrar producto</button>
+                        <button
+                            className="btn btn-cancel"
+                            type="reset"
+                        >Cancelar</button>
+                    </div>
+                </form>
+            </div>
+            <div className="container">
+                {   productos.length <= 0
+                        ?   <p>No hay productos en la lista</p>
+                        :   <table className= "tproducts">
+                                <thead className ="theadproducts">
+                                    <tr className="trproducts">
+                                    <th className ="thproducts">Precio unitario</th>
+                                    <th className ="thproducts">Cantidad</th>
+                                    <th className ="thproducts">Descripción producto</th>
+                                    <th className ="thproducts">estado</th>
+                                    <th className ="thproducts">Acción</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {   productos.map( producto =>
+                                        <tr className="trproducts">
+                                            <td className ="tdproducts">{producto.preciounidad}</td>
+                                            <td className ="tdproducts">{producto.cantidad}</td>
+                                            <td className ="tdproducts">{producto.Descripcion}</td>
+                                            <td className ="tdproducts">{producto.estado}</td>
+                                            <td className ="tdproducts"><button>borrar</button><button>Actualizar</button></td>
+                                        </tr>
+                                    )}
+                                </tbody>
+                            </table>
+                }
+            </div>
+        </>
+    )
 }
 
 export default ProductPage;
