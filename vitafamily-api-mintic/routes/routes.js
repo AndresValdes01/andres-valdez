c402b04aaf759e9776049d778aff466ba54baa3d
AndresValdes01 <90652412+AndresValdes01@users.noreply.github.com>
2021-10-26 22:15:52 -0500

    Se modifican rutas de ventas

--- vitafamily-api-mintic/routes/routes.js
+++ vitafamily-api-mintic/routes/routes.js
@@ -10,16 +10,17 @@ api.get('/usuarios', UsuarioCtrl.getUsuarios);
 api.post('/usuarios', UsuarioCtrl.registrarUsuario);
 
 api.get('/gestionventas', VentasCtrl.getVentas);
+api.get('/gestionventas/:id', VentasCtrl.getOnlySale);
+api.get('/gestionventas/:key/:value', VentasCtrl.getManySales);
 api.post('/gestionventas', VentasCtrl.registrarVenta);
 api.put('/gestionventas', VentasCtrl.updateVenta);
-api.delete('/gestionventas', VentasCtrl.deleteVenta);
+api.delete('/gestionventas/:id', VentasCtrl.deleteVenta);
 
 api.get('/productos', ProductosCtrl.getProducto);
 api.post('/productos', ProductosCtrl.registrarProducto);
 
 api.get('/productos/:id', ProductosCtrl.getOnlyProduct);
-
-api .put( '/productos/:producto_id', ProductosCtrl .updateProducto );
-api .delete( '/productos/:producto_id', ProductosCtrl .deleteProducto );
+api.put( '/productos/:producto_id', ProductosCtrl .updateProducto );
+api.delete( '/productos/:producto_id', ProductosCtrl .deleteProducto );
 
 module.exports = api;
